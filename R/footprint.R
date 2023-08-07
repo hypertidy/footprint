b331b943e51cf144b99037deaac3e70b4fd4d84e
Michael Sumner <mdsumner@gmail.com>
2023-08-07 21:28:43 +1000

    update package

--- R/footprint.R
+++ R/footprint.R
@@ -1,5 +1,5 @@
 
-##extent(range(c(stxy[,1], enxy[,1])), range(c(stxy[,2], enxy[,2]))) + diam * 2
+## vaster::buffer_extent
 buf <- function(d, b, n = 0) {
   ((d %/% b) + n) * b
 }
@@ -7,18 +7,18 @@ buf <- function(d, b, n = 0) {
 #' Build large parent raster
 #'
 #' Create a large sparse raster from an extent and resolution. This grid is never meant to be populated with data
-#' but used as a specification for smaller child windows that are aligned exactly within this larger, high-resolution parent. 
-#' 
-#' Currently the resolution is required to be square. 
+#' but used as a specification for smaller child windows that are aligned exactly within this larger, high-resolution parent.
+#'
+#' Currently the resolution is required to be square.
 #' @param x extent
 #' @param rr resolution
 #'
-#' @return RasterLayer
+#' @return SpatRaster
 #' @export
 #'
 buildparent <- function(x, rr) {
   ## x the extent of all data
-  ext <- extent(buf(xmin(x), rr),
+  ext <- ext(buf(xmin(x), rr),
                 buf(xmax(x), rr, 1),
                 buf(ymin(x), rr),
                 buf(ymax(x), rr, 1))
@@ -26,18 +26,18 @@ buildparent <- function(x, rr) {
   ##xl <- ((range(x[,1] + diam * c(-1, 1)) %/% rr) + c(-1, 1) ) * rr
   ##  yl <- ((range(x[,2] + diam * c(-1, 1)) %/% rr) + c(-1, 1) ) * rr
 
-  x <- raster(ext)
+  x <- rast(ext)
   res(x) <- rr
   x
 }
 extent.psp <- function(x) {
-  extent(as.owin(x)$xrange, as.owin(x)$yrange)
+  ext(as.owin(x)$xrange, as.owin(x)$yrange)
 }
 
 
-#' Build a point or line segment pattern object within a raster window. 
-#' 
-#' This point or line segment pattern is designed to be a child window from a large raster parent, sharing exact alignment and resolution. 
+#' Build a point or line segment pattern object within a raster window.
+#'
+#' This point or line segment pattern is designed to be a child window from a large raster parent, sharing exact alignment and resolution.
 #'
 #' Function \code{bppp} is used for point patterns, \code{bpsp} for line segment patterns
 #' @param x1 coordinate, x,y two values
@@ -48,15 +48,15 @@ extent.psp <- function(x) {
 #' @return spatstat point or line segment pattern with built-in raster window
 #' @export
 #'
-#' @importFrom raster extent res crop 
-#' @importFrom spatstat owin psp ppp
+#' @importFrom raster extent res crop
+#' @importFrom spatstat.geom owin psp ppp
 bppp <- function(x1, pare, diam = 0) {
   x <- x1
-  e <- extent(x[1] + c(-1, 1) *  diam, x[2] + c(-1, 1) *  diam)
+  e <- ext(x[1] + c(-1, 1) *  diam, x[2] + c(-1, 1) *  diam)
   rr <- res(pare)[1]
   ## xl <- ((range(x[,1] + diam * c(-1, 1)) %/% rr) + c(-1, 1) ) * rr
   ##  yl <- ((range(x[,2] + diam * c(-1, 1)) %/% rr) + c(-1, 1) ) * rr
-  ext <- extent(buf(xmin(e), rr),
+  ext <- ext(buf(xmin(e), rr),
                 buf(xmax(e), rr, 1),
                 buf(ymin(e), rr),
                 buf(ymax(e), rr, 1))
@@ -73,12 +73,13 @@ bppp <- function(x1, pare, diam = 0) {
 #' @export
 #' @rdname bppp
 bpsp <- function(x1, x2, pare, diam = 0) {
+
   x <- rbind(x1, x2)
-  e <- extent(t(x))  ## ouch, two rows!
+  e <- ext(sort(c(x[, 1])),sort( x[,2]))
   rr <- res(pare)[1]
   ## xl <- ((range(x[,1] + diam * c(-1, 1)) %/% rr) + c(-1, 1) ) * rr
   ##  yl <- ((range(x[,2] + diam * c(-1, 1)) %/% rr) + c(-1, 1) ) * rr
-  ext <- extent(buf(xmin(e), rr),
+  ext <- ext(buf(xmin(e), rr),
                 buf(xmax(e), rr, 1),
                 buf(ymin(e), rr),
                 buf(ymax(e), rr, 1))
